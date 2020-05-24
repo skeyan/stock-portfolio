63fc09953e3165dd7a969b184a419c0a09d48afe
Eva Yan <eyan0749@gmail.com>
2020-05-24 15:40:35 -0400

    Fixes #1

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,6 @@ class Home extends Component {
     }
 
     render() {
-        // console.log(this.state.money);
         return (
             <div>
                 <center><h1>Home Test</h1></center>
