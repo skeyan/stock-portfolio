79859577c43ca757f951bdca907b134092843030
Eva Yan <eyan0749@gmail.com>
2020-05-26 02:12:46 -0400

    Added user schema and exported user model

---
+++ back-end/models/Stock.js
@@ -0,0 +1,7 @@
+/*
+    * This file contains the schema and model for a single Stock (that has been bought).
+    * A stock (that has been bought) will have the following information stored:
+    * - stock symbol/name to identify it
+    * - email (user) who bought the stock
+    * - quantity that the user bought
+*/
