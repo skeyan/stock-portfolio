45523c4fc9385f5b12d64cdef4da64b289444f24
Eva Yan <eyan0749@gmail.com>
2020-05-26 21:50:59 -0400

    Added routing for Login

--- back-end/routes/UserRoute.js
+++ back-end/routes/UserRoute.js
@@ -2,52 +2,97 @@ var express = require ('express');
 var router = express.Router();
 var User = require ('../models/User.js');
 
-// Handle post requests from the front end with a payload
-// Used to register a user
-router.post("/register", function (req, res) {
+// Hash and compare passwords with the bcrypt library
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// Handle GET requests from the front end with a payload to the /login path
+// Compares the user's password with bcrypt
+// Used to login a user
+    // Check if user's email exists in database
+    // If so, use bcrypt to compare passwords
+router.get("/login", function (req, res) {
     // Check if the user (email) already exists in the database
     // Go through all the users in the User table to see if it exists
-    // Success: emails contains all of the matches
-    // Failure: err contains error
-    User.find({ email: req.body.email }, (err, emails) => {
-        console.log("EMAILS: ", emails);
-        if (err) {
-            console.log(err);
+    User.findOne({ email: req.body.email }, (err, user) => {
+        console.log("email:", user);
+        if (err) { // find error --> cannot login
             res.send({
                 success: false,
                 message: err // send the error object
             })
         }
-        if (emails.length > 0) { // emails exist
-            console.log("EMAIL ALREADY EXISTS, SHOULD NOT REGISTER NEW USER");
+        if (!user) { // user (email) doesn't exist in database, not yet registered
+            res.send({
+                success: false,
+                message: "User does not exist."
+            })
+        }
+        else { // user (email) exists in database, so move onto compare passwords now
+            bcrypt.compare(req.body.password, user.password, function(bErr, bRes) {
+                if(bRes) { // Passwords match -> tell frontend to login the user.
+                    res.send({
+                        success: true,
+                        message: "Successful login."
+                    })
+                }
+                else { // Passwords don't match -> tell frontend to NOT login the user.
+                    res.send({
+                        success: false,
+                        message: "Wrong password."
+                    })
+                }
+            })
+        }
+    })
+})
+
+// Handle POST requests from the front end with a payload to the /register path
+// Hashes the user's password with bcrypt
+// Used to register a user
+    // Check if entered email exists in database
+    // If not, register new user with that email
+router.post("/register", function (req, res) {
+    // Check if the user (email) already exists in the database
+    // Go through all the users in the User table to see if it exists
+    User.find({ email: req.body.email }, (err, emails) => {
+        // console.log("EMAILS: ", emails);
+        if (err) { // find error -> cannot register
+            res.send({
+                success: false,
+                message: err // send the error object
+            })
+        }
+        if (emails.length > 0) { // email already exists -> cannot register
             res.send({
                 success: false,
                 message: "Unsuccessful registration, email already exists."
             })
         } 
-        else {
-            console.log("EMAIL DOESN'T EXIST YET, CAN REGISTER NEW USER");
-            // Create an instance (document) of a user model 
-            let user = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                cashBalance: req.body.cashBalance
-            });
+        else { // email doesn't exist yet -> can register the new user
+            // Hash their password asynchronously, create user, and store in database.
+            bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+                // Create an instance (document) of a user model 
+                let user = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hash,
+                    cashBalance: req.body.cashBalance
+                });
 
-            // Save the new user into the database
-            user.save(err => { 
-                if (err) console.log(err);
-            })
-            res.send({
-                success: true,
-                message: "Successful registration, new user created."
-            })
+                // Save the new user into the database
+                user.save(err => { 
+                    if (err) console.log(err);
+                })
+
+                // Send a object message to alert the front-end of what happened here
+                res.send({
+                    success: true,
+                    message: "Successful registration, new user created."
+                })
+            });
         }
     }) 
-    // console.log(user);
-    // res.end();
-    // res.send(req.body);
 }) 
 
 module.exports = router;
