31f46da7c556156d39f8b196a89d49a769b45573
Eva Yan <eyan0749@gmail.com>
2020-05-28 00:37:53 -0400

    Styled the Transactions page

--- front-end/src/views/TransactionsView.jsx
+++ front-end/src/views/TransactionsView.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import '../styles/Transactions.css';
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { ListGroup, ListGroupItem, PageHeader} from "react-bootstrap";
 
 import "../styles/Transactions.css";
 
@@ -12,17 +12,21 @@ class TransactionsView extends Component {
         // 2. if logged in, with no transactions
         // 3. not logged in
         return (
-            <div>
-                {this.props.numTransactions > 0 ? (
+            <div className="transactions-container">
+                <PageHeader>Transactions History</PageHeader>
+                <ListGroup>
+                { this.props.numTransactions > 0 ? (
                     this.props.transactionsArray.map((transaction, i) => (
-                        <li key={i}>{transaction.tickerSymbol}</li>
+                        <ListGroupItem key={i}><b>{transaction.time.toString().substring(0, 10)}</b> - BUY ({transaction.tickerSymbol.toUpperCase()}) - {transaction.quantity} Shares @ ${transaction.totalCost}</ListGroupItem>
                     ))
                 ) : this.props.loggedIn ? (
-                    <h3>You have no transactions.</h3>
+                    <h3>You have no transactions. Visit your <Link to="/portfolio">portfolio</Link> to buy stocks.
+                    </h3>
                 ) : (
-                    <h3>Please log in to view your transactions.</h3>
+                    <h3>Please <Link to="/login">log in</Link> to view your transaction history.</h3>
                 )
-            }
+                }
+                </ListGroup>
             </div>
         );
     }
